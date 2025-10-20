834b184c89d08312847ada64701b826ddedb2d99
jgOhYeah <koyugaDev@gmail.com>
2025-10-20 22:09:54 +1100

    Started GUI

---
+++ app/gui.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from typing import Self
+import ttkbootstrap as ttk
+import ttkbootstrap.constants as ttkc
+from abc import ABC, abstractmethod
+
+class AppTab(ABC):
+    """Base class for tabs."""
+    def __init__(self, root:ttk.Notebook, tab_name:str) -> None:
+        """Creates a frame and adds it to the notebook with the provided name."""
+        self._frame = ttk.Frame(root)
+        root.add(self._frame, text=tab_name)
+
+class EventsTab(AppTab):
+    """Class to handle the events tab."""
+    def __init__(self, root:ttk.Notebook) -> None:
+        super().__init__(root, "Events")
+
+        # Select event dropdown.
+        ttk.Label(self._frame, text="Selected event").grid(row=0, column=0)
+        event_list = ["2024 Test", "2025 Test", "2026 Test"]
+        selected_event_str = tk.StringVar(self._frame, "")
+        event_dropdown = ttk.OptionMenu(self._frame, selected_event_str, event_list[0], *event_list)
+        event_dropdown.grid(row=0, column=1)
+
+        # Add new event button.
+        ttk.Button(self._frame, text="Add new event").grid(row=0, column=3)
+
+        self._frame.columnconfigure(2, weight=1)
+
+class CarsTab(AppTab):
+    """Class to handle the cars tab."""
+    def __init__(self, root:ttk.Notebook) -> None:
+        super().__init__(root, "Cars")
+
+class RoundRobinTab(AppTab):
+    """Class to handle the round robin tab."""
+    def __init__(self, root:ttk.Notebook) -> None:
+        super().__init__(root, "Round robin")
+
+class KnockoutTab(AppTab):
+    """Class to handle the knockout round tab."""
+    def __init__(self, root:ttk.Notebook) -> None:
+        super().__init__(root, "Knockout")
+
+class Gui:
+    def __init__(self) -> None:
+        self._root = ttk.Window(title="Solar car draw generator", themename="cosmo")
+        # self._root = tk.Tk()
+        notebook = ttk.Notebook(self._root)
+        self._events = EventsTab(notebook)
+        self._cars = CarsTab(notebook)
+        self._round_robin = RoundRobinTab(notebook)
+        self._knockout = KnockoutTab(notebook)
+        notebook.pack(expand=True, fill=ttkc.BOTH)
+
+    def run(self) -> None:
+        self._root.mainloop()
